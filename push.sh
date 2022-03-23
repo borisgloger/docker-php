447369d6548c3670c7396d4d390c8f639b03e90a
Andreas de Pretis <andreas@depretis.at>
2022-03-23 18:33:36 +0100

    Refactor for borisgloger and add PHP 7.3

--- push.sh
+++ push.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker push borisgloger/php-nginx:7.3
-docker push borisgloger/php-fpm:7.3
+docker push ghcr.io/borisgloger/docker-php/php-nginx:7.3
+#docker push ghcr.io/borisgloger/docker-php/php-fpm:7.3
